1706ca1f6b321bb500a95b01f16594afc74f0c00
pricejamesw <james.price2@gmail.copm>
2021-12-16 14:02:56 -0600

    Robot JWP04

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -22,6 +22,9 @@ public class Robot extends TimedRobot {
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
+
+   // JWP04
+   
   @Override
   public void robotInit() {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
